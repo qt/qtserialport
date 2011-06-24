b2c7379ea435185be54776ddb54c095c7520d618
Sergey Belyashov <Sergey.Belyashov@gmail.com>
2011-06-24 11:39:25 +0400

    Removed spaces from end of lines

--- include/serialport.h
+++ include/serialport.h
@@ -1,7 +1,7 @@
 /*
     License...
 */
- 
+
 #ifndef SERIALPORT_H
 #define SERIALPORT_H
 
@@ -16,10 +16,10 @@
 #else
 #  define SERIALPORT_EXPORT
 #endif
- 
+
 class SerialPortInfo;
 class SerialPortPrivate;
- 
+
 class SERIALPORT_EXPORT SerialPort : public QIODevice
 {
     Q_OBJECT
@@ -33,168 +33,168 @@ class SERIALPORT_EXPORT SerialPort : public QIODevice
     Q_PROPERTY(bool dtr READ dtr WRITE setDtr)
     Q_PROPERTY(bool rts READ rts WRITE setRts)
     Q_PROPERTY(PortError error READ error RESET unsetError)
- 
+
     Q_ENUMS( Directions Rate DataBits Parity StopBits FlowControl Lines DataErrorPolicy PortError ) 
- 
+
 public:
- 
-    enum Direction  { 
-        Input = 1, 
+
+    enum Direction  {
+        Input = 1,
         Output = 2,
         AllDirections = Input | Output
     };
     Q_DECLARE_FLAGS(Directions, Direction)
- 
+
     enum Rate {
-        Rate1200 = 1200, 
-        Rate2400 = 2400, 
-        Rate4800 = 4800,   
-        Rate9600 = 9600, 
-        Rate19200 = 19200, 
-        Rate38400 = 38400,  
-        Rate57600 = 57600, 
-        Rate115200 = 115200, 
-        UnknownRate = -1 
+        Rate1200 = 1200,
+        Rate2400 = 2400,
+        Rate4800 = 4800,
+        Rate9600 = 9600,
+        Rate19200 = 19200,
+        Rate38400 = 38400,
+        Rate57600 = 57600,
+        Rate115200 = 115200,
+        UnknownRate = -1
     };
- 
+
     enum DataBits {
-        Data5 = 5, 
-        Data6 = 6, 
-        Data7 = 7, 
-        Data8 = 8, 
-        UnknownDataBits = -1 
+        Data5 = 5,
+        Data6 = 6,
+        Data7 = 7,
+        Data8 = 8,
+        UnknownDataBits = -1
     };
- 
+
     enum Parity {
-        NoParity = 0, 
-        EvenParity = 2, 
-        OddParity = 3,        
-        SpaceParity = 4, 
-        MarkParity = 5,         
-        UnknownParity = -1 
+        NoParity = 0,
+        EvenParity = 2,
+        OddParity = 3,
+        SpaceParity = 4,
+        MarkParity = 5,
+        UnknownParity = -1
     };
- 
+
     enum StopBits {
-        OneStop = 1, 
-        OneAndHalfStop = 3, 
-        TwoStop = 2, 
-        UnknownStopBits = -1 
+        OneStop = 1,
+        OneAndHalfStop = 3,
+        TwoStop = 2,
+        UnknownStopBits = -1
     };
- 
+
     enum FlowControl {
-        NoFlowControl,      
-        HardwareControl, 
-        SoftwareControl, 
-        UnknownFlowControl = -1 
+        NoFlowControl,
+        HardwareControl,
+        SoftwareControl,
+        UnknownFlowControl = -1
     };
- 
+
     enum Line {
-        Le = 0x01, 
-        Dtr = 0x02, 
-        Rts = 0x04, 
-        St = 0x08, 
-        Sr = 0x10, 
-        Cts = 0x20, 
-        Dcd = 0x40, 
-        Ri = 0x80, 
-        Dsr = Le    
+        Le = 0x01,
+        Dtr = 0x02,
+        Rts = 0x04,
+        St = 0x08,
+        Sr = 0x10,
+        Cts = 0x20,
+        Dcd = 0x40,
+        Ri = 0x80,
+        Dsr = Le
     };
     Q_DECLARE_FLAGS(Lines, Line)
- 
+
     enum DataErrorPolicy {
         SkipPolicy,
         PassZeroPolicy,
         IgnorePolicy,
         StopReceivingPolicy
     };
- 
+
     enum PortError {
-        NoError,                
-        NoSuchDeviceError,           
-        PermissionDeniedError,       
-        DeviceAlreadyOpenedError,    
-        DeviceIsNotOpenedError,      
+        NoError,
+        NoSuchDeviceError,
+        PermissionDeniedError,
+        DeviceAlreadyOpenedError,
+        DeviceIsNotOpenedError,
         ParityError,
         IoError
     };
- 
+
     explicit SerialPort(QObject *parent = 0);
     explicit SerialPort(const QString &name, QObject *parent = 0);
     explicit SerialPort(const SerialPortInfo &info, QObject *parent = 0);
     virtual ~SerialPort();
- 
+
     void setPort(const QString &port);
     void setPort(const SerialPortInfo &info);
     QString portName() const;
- 
+
     virtual bool open(OpenMode mode);
     virtual void close();
- 
+
     bool setRate(qint32 rate, Directions dir = AllDirections);
     qint32 rate(Directions dir = AllDirections) const;
- 
+
     bool setDataBits(DataBits dataBits);
     DataBits dataBits() const;
- 
+
     bool setParity(Parity parity);
     Parity parity() const;
- 
+
     bool setStopBits(StopBits stopBits);
     StopBits stopBits() const;
- 
+
     bool setFlowControl(FlowControl flow);
     FlowControl flowControl() const;
- 
+
     bool setDataInterval(int usecs);
     int dataInterval() const;
- 
+
     bool setReadTimeout(int msecs);
     int readTimeout() const;
- 
+
     bool dtr() const;
     bool rts() const;
- 
+
     Lines lines() const;
- 
+
     bool flush();
     virtual bool reset();
- 
+
     bool setDataErrorPolicy(DataErrorPolicy policy = IgnorePolicy);
     DataErrorPolicy dataErrorPolicy() const;
- 
+
     PortError error() const;
     void unsetError();
- 
+
     virtual bool isSequential() const;
- 
+
     virtual qint64 bytesAvailable() const;
     virtual qint64 bytesToWrite() const;
- 
+
     virtual bool waitForReadyRead(int msecs);
     virtual bool waitForBytesWritten(int msecs);
- 
- public Q_SLOTS:
+
+public Q_SLOTS:
     bool setDtr(bool set);
     bool setRts(bool set);
     bool sendBreak(int duration = 0);
     bool setBreak(bool set = true);
     bool clearBreak(bool clear = true);
- 
+
 protected:
     virtual qint64 readData(char *data, qint64 maxSize);
     virtual qint64 writeData(const char *data, qint64 maxSize);
- 
+
 private:
     SerialPortPrivate * const d_ptr;
- 
+
     Q_DECLARE_PRIVATE(SerialPort)
     Q_DISABLE_COPY(SerialPort)
 };
- 
+
 inline bool SerialPort::clearBreak(bool clear)
 { return setBreak(!clear); }
- 
+
 Q_DECLARE_OPERATORS_FOR_FLAGS(SerialPort::Directions)
 Q_DECLARE_OPERATORS_FOR_FLAGS(SerialPort::Lines)
- 
+
 #endif // SERIALPORT_H
