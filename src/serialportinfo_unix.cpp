71389ed2a6f862a6baf96037a8da386dc56b966d
kuzulis <scapig2@yandex.ru>
2011-08-12 23:14:03 +0400

    For a class SerialPortInfo on Linux added the ability to determine the available ports without using Udev.

--- src/serialportinfo_unix.cpp
+++ src/serialportinfo_unix.cpp
@@ -4,30 +4,40 @@
 
 #include "serialportinfo.h"
 #include "serialportinfo_p.h"
+#include "ttylocker_p_unix.h"
 
 #include <sys/types.h>
 #include <signal.h>
 #include <unistd.h>
 #include <errno.h>
 
+#define HAVE_UDEV
+
+#if defined (HAVE_UDEV)
 extern "C"
 {
-#include <libudev.h>
+#  include <libudev.h>
 }
+#else
+#  include <QDir>
+#endif
 
 #include <QtCore/QVariant>
 #include <QtCore/QStringList>
+#include <QtCore/QRegExp>
 
-//If OS is GNU/Linux
 static QStringList nameFilters()
 {
     static QStringList list;
-    list << "ttyS"    /* standart UART 8250 and etc. */
-         << "ttyUSB"  /* usb/serial converters PL2303 and etc. */
-         << "ttyACM"  /* CDC_ACM converters (i.e. Mobile Phones). */
-         << "ttyMI"   /* MOXA pci/serial converters. */
-         << "rfcomm"; /* Bluetooth serial device. */
-
+#if defined (Q_OS_LINUX)
+    list << "ttyS*"    /* Standart UART 8250 and etc. */
+         << "ttyUSB*"  /* Usb/serial converters PL2303 and etc. */
+         << "ttyACM*"  /* CDC_ACM converters (i.e. Mobile Phones). */
+         << "ttyMI*"   /* MOXA pci/serial converters. */
+         << "rfcomm*"; /* Bluetooth serial device. */
+#else
+    // Here for other *nix OS.
+#endif
     return list;
 }
 
@@ -39,6 +49,8 @@ QList<SerialPortInfo> SerialPortInfo::availablePorts()
 {
     QList<SerialPortInfo> ports;
 
+#if defined (HAVE_UDEV)
+    // With udev scan.
     struct udev *udev = ::udev_new();
     if (udev) {
 
@@ -59,24 +71,29 @@ QList<SerialPortInfo> SerialPortInfo::availablePorts()
                 if (udev_device) {
 
                     SerialPortInfo info;
-                    //get device name
+                    // Get device name.
                     QString s(::udev_device_get_devnode(udev_device));
 
                     foreach (QString mask, nameFilters()) {
 
-                        if (s.contains(mask)) {
-                            //name
-                            info.d_ptr->portName = s; //< Here add regexp, e.g. for delete /dev/
-                            //location
+                        if (s.contains(mask.remove("*"))) {
+                            // Location.
                             info.d_ptr->device = s;
-                            //description
+                            // Name.
+                            info.d_ptr->portName = s.remove(QRegExp("/[\\w|\\d|\\s]+/"));
+                            // Description.
                             info.d_ptr->description = QString(::udev_device_get_property_value(udev_device,
                                                                                                "ID_MODEL_FROM_DATABASE"));
-                            //manufacturer
+                            if (info.d_ptr->description.isEmpty())
+                                info.d_ptr->description = QString(QObject::tr("Unknown."));
+                            // Manufacturer.
                             info.d_ptr->manufacturer = QString(::udev_device_get_property_value(udev_device,
                                                                                                 "ID_VENDOR_FROM_DATABASE"));
+                            if (info.d_ptr->manufacturer.isEmpty())
+                                info.d_ptr->manufacturer = QString(QObject::tr("Unknown."));
 
                             ports.append(info);
+                            break;
                         }
                     }
                     ::udev_device_unref(udev_device);
@@ -90,6 +107,32 @@ QList<SerialPortInfo> SerialPortInfo::availablePorts()
 
     if (udev)
         ::udev_unref(udev);
+#else
+    // With directory /dev scan.
+    QDir devDir("/dev");
+    if (devDir.exists()) {
+
+        devDir.setNameFilters(nameFilters());
+        devDir.setFilter(QDir::Files | QDir::System);
+
+        foreach(QFileInfo fi, devDir.entryInfoList()) {
+            if (!fi.isDir()) {
+                SerialPortInfo info;
+                // Location.
+                info.d_ptr->device = fi.absoluteFilePath();
+                // Name.
+                info.d_ptr->portName = info.d_ptr->device;
+                info.d_ptr->portName.remove(QRegExp("/[\\w|\\d|\\s]+/"));
+                // Description.
+                info.d_ptr->description = QString(QObject::tr("Unknown."));
+                // Manufacturer.
+                info.d_ptr->manufacturer = QString(QObject::tr("Unknown."));
+
+                ports.append(info);
+            }
+        }
+    }
+#endif
 
     return ports;
 }
@@ -103,11 +146,9 @@ QList<int> SerialPortInfo::standardRates() const
 
 bool SerialPortInfo::isBusy() const
 {
-    // Try open and close port by location: systemLocation();
-    //...
-    //...
-
-    return false;
+    bool currPid = false;
+    bool ret = TTYLocker::isLocked(systemLocation(), &currPid);
+    return ret;
 }
 
 bool SerialPortInfo::isValid() const
