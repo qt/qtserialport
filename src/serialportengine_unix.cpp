338cf76fc0b18778f574026e27c3689e0e640c06
Denis Shienkov <scapig@yandex.ru>
2012-05-27 14:02:33 +0400

    Reduction to the general style of the code at the static initialization of structs
    Added missing space after opening ace '{' and before closing brace '}'.
    
    Change-Id: I281928b7d957d7e7f6f41f6c1ab97ed3f253bc2b
    Reviewed-by: Laszlo Papp <lpapp@kde.org>
    Reviewed-by: Denis Shienkov <scapig@yandex.ru>

--- src/serialportengine_unix.cpp
+++ src/serialportengine_unix.cpp
@@ -1085,7 +1085,7 @@ static const RatePair *standardRatesTable_end =
 */
 qint32 UnixSerialPortEngine::rateFromSetting(qint32 setting)
 {
-    const RatePair rp = {0, setting};
+    const RatePair rp = { 0, setting };
     const RatePair *ret = qFind(standardRatesTable, standardRatesTable_end, rp);
     return (ret != standardRatesTable_end) ? ret->rate : 0;
 }
@@ -1096,7 +1096,7 @@ qint32 UnixSerialPortEngine::rateFromSetting(qint32 setting)
 */
 qint32 UnixSerialPortEngine::settingFromRate(qint32 rate)
 {
-    const RatePair rp = {rate, 0};
+    const RatePair rp = { rate, 0 };
     const RatePair *ret = qBinaryFind(standardRatesTable, standardRatesTable_end, rp);
     return (ret != standardRatesTable_end) ? ret->setting : 0;
 }
