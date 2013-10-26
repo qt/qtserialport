2693e8ee98dd1a6cf22c24eb20b7ede71409a908
Laszlo Papp <lpapp@kde.org>
2013-10-26 13:06:40 +0100

    Add a new simple and command line async reader example
    Change-Id: I4142d187c3ad37d1e7f5219df3d50679a9f35a8e
    Reviewed-by: Sergey Belyashov <Sergey.Belyashov@gmail.com>
    Reviewed-by: Denis Shienkov <denis.shienkov@gmail.com>

---
+++ examples/serialport/creaderasync/main.cpp
@@ -0,0 +1,104 @@
+/****************************************************************************
+**
+** Copyright (C) 2013 Laszlo Papp <lpapp@kde.org>
+** Contact: http://www.qt-project.org/legal
+**
+** This file is part of the QtSerialPort module of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and Digia.  For licensing terms and
+** conditions see http://qt.digia.com/licensing.  For further information
+** use the contact form at http://qt.digia.com/contact-us.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Digia gives you certain additional
+** rights.  These rights are described in the Digia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3.0 as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU General Public License version 3.0 requirements will be
+** met: http://www.gnu.org/copyleft/gpl.html.
+**
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#include "serialportreader.h"
+
+#include <QtSerialPort/QSerialPort>
+
+#include <QTextStream>
+#include <QCoreApplication>
+#include <QFile>
+#include <QStringList>
+
+QT_USE_NAMESPACE
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication coreApplication(argc, argv);
+    int argumentCount = QCoreApplication::arguments().size();
+    QStringList argumentList = QCoreApplication::arguments();
+
+    QTextStream standardOutput(stdout);
+
+    if (argumentCount == 1) {
+        standardOutput << QObject::tr("Usage: %1 <serialportname> [baudrate]").arg(argumentList.first()) << endl;
+        return 1;
+    }
+
+    QSerialPort serialPort;
+    QString serialPortName = argumentList.at(1);
+    serialPort.setPortName(serialPortName);
+
+    if (!serialPort.open(QIODevice::ReadOnly)) {
+        standardOutput << QObject::tr("Failed to open port %1, error: %2").arg(serialPortName).arg(serialPort.errorString()) << endl;
+        return 1;
+    }
+
+    int serialPortBaudRate = (argumentCount > 2) ? argumentList.at(2).toInt() : QSerialPort::Baud9600;
+    if (!serialPort.setBaudRate(serialPortBaudRate)) {
+        standardOutput << QObject::tr("Failed to set 9600 baud for port %1, error: %2").arg(serialPortName).arg(serialPort.errorString()) << endl;
+        return 1;
+    }
+
+    if (!serialPort.setDataBits(QSerialPort::Data8)) {
+        standardOutput << QObject::tr("Failed to set 8 data bits for port %1, error: %2").arg(serialPortName).arg(serialPort.errorString()) << endl;
+        return 1;
+    }
+
+    if (!serialPort.setParity(QSerialPort::NoParity)) {
+        standardOutput << QObject::tr("Failed to set no parity for port %1, error: %2").arg(serialPortName).arg(serialPort.errorString()) << endl;
+        return 1;
+    }
+
+    if (!serialPort.setStopBits(QSerialPort::OneStop)) {
+        standardOutput << QObject::tr("Failed to set 1 stop bit for port %1, error: %2").arg(serialPortName).arg(serialPort.errorString()) << endl;
+        return 1;
+    }
+
+    if (!serialPort.setFlowControl(QSerialPort::NoFlowControl)) {
+        standardOutput << QObject::tr("Failed to set no flow control for port %1, error: %2").arg(serialPortName).arg(serialPort.errorString()) << endl;
+        return 1;
+    }
+
+    SerialPortReader serialPortReader(&serialPort);
+
+    return coreApplication.exec();
+}
--- examples/serialport/serialport.pro
+++ examples/serialport/serialport.pro
@@ -1,6 +1,6 @@
 TEMPLATE = subdirs
 CONFIG += ordered
-SUBDIRS = cenumerator creadersync cwriterasync cwritersync
+SUBDIRS = cenumerator creaderasync creadersync cwriterasync cwritersync
 greaterThan(QT_MAJOR_VERSION, 4) {
     !isEmpty(QT.widgets.name):SUBDIRS += enumerator terminal blockingmaster blockingslave
 } else {
