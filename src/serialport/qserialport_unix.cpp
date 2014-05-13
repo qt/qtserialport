qt/qtserialport

commit 7c737e0edcb93585856c65890ef34e5c5a28ee6b

Make it possible to set the port values before opening
This patch also changes the behavior of the open method. We do not use port
detection anymore for good. It has been a broken concept, and it is very
unlikely that anyone has ever relied on it. If anyone had done that, they would
be in trouble anyway, getting noisy warnings needlessly and all that.

The other option was also considered to keep this behavior optionally as the
default, but that would complicate the API too much without not much gain.

The default port settings are now also the sane 9600,8,N,1, and no flow control.
Also please note that the serial port is closed automatically in the open method
if any of the settings fails.

Several subsequent changes can follow this one, namely:

* Changing the examples to show this usage.

* Add a QSerialPortSettings data structure as it seems to be common currently in
client codes.

* Add the QML plugin with only thin wrapper declarative classes and all that.

* Deprecate the restore settings on close API.

* Remove the internal current and restored data representations.

* Add further error values to the API for the fine tuning of the error reporting
when there are setting errors on open.

* Add proper test coverage.

In short, this change opens the gate up for many interesting subsequent changes
that would be useful for the module overall.

In general, this change makes it possible to reach the feature parity with
QextSerialPort so that we are not much worse anymore. We could do the same that
QextSerialPort could do many years ago, already.

Task-number: QTBUG-33774

Change-Id: I8527672f8b0bb2581fa7054ccffda66ab7fa4a85
Reviewed-by: Sergey Belyashov <Sergey.Belyashov@gmail.com>

src/serialport/qserialport_unix.cpp
@@ -265,7 +265,6 @@ bool QSerialPortPrivate::open(QIODevice::OpenMode mode)
     if ((flags & O_WRONLY) == 0)
         setReadNotificationEnabled(true);
 
-    detectDefaultSettings();
     return true;
 }
 
@@ -518,6 +517,16 @@ bool QSerialPortPrivate::waitForBytesWritten(int msecs)
             return completeAsyncWrite();
     }
     return false;
+}
+
+bool QSerialPortPrivate::setBaudRate()
+{
+    if (!setBaudRate(inputBaudRate, QSerialPort::Input)
+            && !setBaudRate(outputBaudRate, QSerialPort::Output)) {
+        return false;
+    }
+
+    return true;
 }
 
 bool QSerialPortPrivate::setBaudRate(qint32 baudRate, QSerialPort::Directions directions)
@@ -860,99 +869,6 @@ bool QSerialPortPrivate::updateTermios()
         return false;
     }
     return true;
-}
-
-void QSerialPortPrivate::detectDefaultSettings()
-{
-    // Detect baud rate.
-    const speed_t inputUnixBaudRate = ::cfgetispeed(&currentTermios);
-    const speed_t outputUnixBaudRate = ::cfgetospeed(&currentTermios);
-    bool isCustomBaudRateCurrentSet = false;
-
-#ifdef Q_OS_LINUX
-    // try detect the ability to support custom baud rate
-    isCustomBaudRateSupported = ::ioctl(descriptor, TIOCGSERIAL, &currentSerialInfo) != -1
-            && ::ioctl(descriptor, TIOCSSERIAL, &currentSerialInfo) != -1;
-
-    if (isCustomBaudRateSupported) {
-        restoredSerialInfo = currentSerialInfo;
-
-        // assume that the baud rate is a custom
-        isCustomBaudRateCurrentSet = inputUnixBaudRate == B38400 && outputUnixBaudRate == B38400;
-
-        if (isCustomBaudRateCurrentSet) {
-            if ((currentSerialInfo.flags & ASYNC_SPD_CUST)
-                    && currentSerialInfo.custom_divisor > 0) {
-
-                // yes, speed is really custom
-                inputBaudRate = currentSerialInfo.baud_base / currentSerialInfo.custom_divisor;
-                outputBaudRate = inputBaudRate;
-            } else {
-                // no, we were wrong and the speed is a standard 38400 baud
-                isCustomBaudRateCurrentSet = false;
-            }
-        }
-    }
-#else
-    // other *nix
-#endif
-    if (!isCustomBaudRateSupported || !isCustomBaudRateCurrentSet) {
-        inputBaudRate = QSerialPortPrivate::baudRateFromSetting(inputUnixBaudRate);
-        outputBaudRate = QSerialPortPrivate::baudRateFromSetting(outputUnixBaudRate);
-    }
-
-    // Detect databits.
-    switch (currentTermios.c_cflag & CSIZE) {
-    case CS5:
-        dataBits = QSerialPort::Data5;
-        break;
-    case CS6:
-        dataBits = QSerialPort::Data6;
-        break;
-    case CS7:
-        dataBits = QSerialPort::Data7;
-        break;
-    case CS8:
-        dataBits = QSerialPort::Data8;
-        break;
-    default:
-        qWarning("%s: Unexpected data bits settings", Q_FUNC_INFO);
-        dataBits = QSerialPort::Data8;
-        break;
-    }
-
-    // Detect parity.
-#ifdef CMSPAR
-    if (currentTermios.c_cflag & CMSPAR) {
-        parity = currentTermios.c_cflag & PARODD ?
-                    QSerialPort::MarkParity : QSerialPort::SpaceParity;
-    } else {
-#endif
-        if (currentTermios.c_cflag & PARENB) {
-            parity = currentTermios.c_cflag & PARODD ?
-                        QSerialPort::OddParity : QSerialPort::EvenParity;
-        } else {
-            parity = QSerialPort::NoParity;
-        }
-#ifdef CMSPAR
-    }
-#endif
-
-    // Detect stopbits.
-    stopBits = currentTermios.c_cflag & CSTOPB ?
-                QSerialPort::TwoStop : QSerialPort::OneStop;
-
-    // Detect flow control.
-    if ((!(currentTermios.c_cflag & CRTSCTS)) && (!(currentTermios.c_iflag & (IXON | IXOFF | IXANY))))
-        flowControl = QSerialPort::NoFlowControl;
-    else if ((!(currentTermios.c_cflag & CRTSCTS)) && (currentTermios.c_iflag & (IXON | IXOFF | IXANY)))
-        flowControl = QSerialPort::SoftwareControl;
-    else if ((currentTermios.c_cflag & CRTSCTS) && (!(currentTermios.c_iflag & (IXON | IXOFF | IXANY))))
-        flowControl = QSerialPort::HardwareControl;
-    else {
-        qWarning("%s: Unexpected flow control settings", Q_FUNC_INFO);
-        flowControl = QSerialPort::NoFlowControl;
-    }
 }
 
 QSerialPort::SerialPortError QSerialPortPrivate::decodeSystemError() const

src/serialport/qserialport.cpp
@@ -68,8 +68,8 @@ QSerialPortPrivateData::QSerialPortPrivateData(QSerialPort *q)
     , readBuffer(SERIALPORT_BUFFERSIZE)
     , writeBuffer(SERIALPORT_BUFFERSIZE)
     , error(QSerialPort::NoError)
-    , inputBaudRate(0)
-    , outputBaudRate(0)
+    , inputBaudRate(9600)
+    , outputBaudRate(9600)
     , dataBits(QSerialPort::Data8)
     , parity(QSerialPort::NoParity)
     , stopBits(QSerialPort::OneStop)
@@ -499,9 +499,9 @@ QString QSerialPort::portName() const
     successful; otherwise returns false and sets an error code which can be
     obtained by calling the error() method.
 
-    \note This method has to be called before setting certain serial port
-    parameters. See each property documentation for the details of when it is
-    necessary.
+    \note The method returns false if opening the port is successful, but could
+    not set any of the port settings successfully. In that case, the port is
+    closed automatically not to leave the port around with incorrect settings.
 
     \warning The \a mode has to be QIODevice::ReadOnly, QIODevice::WriteOnly,
     or QIODevice::ReadWrite. Other modes are unsupported.
@@ -530,8 +530,16 @@ bool QSerialPort::open(OpenMode mode)
 
     QIODevice::open(mode);
 
-    d->dataTerminalReady = isDataTerminalReady();
-    d->requestToSend = isRequestToSend();
+    if (!d->setBaudRate()
+        || !d->setDataBits(d->dataBits)
+        || !d->setParity(d->parity)
+        || !d->setStopBits(d->stopBits)
+        || !d->setFlowControl(d->flowControl)
+        || !d->setDataTerminalReady(d->dataTerminalReady)
+        || !d->setRequestToSend(d->requestToSend)) {
+        close();
+        return false;
+    }
 
     return true;
 }
@@ -596,35 +604,29 @@ bool QSerialPort::settingsRestoredOnClose() const
     \property QSerialPort::baudRate
     \brief the data baud rate for the desired direction
 
-    If the setting is successful, returns true; otherwise returns false and sets
-    an error code which can be obtained by accessing the value of the
-    QSerialPort::error property. To set the baud rate, use the enumeration
-    QSerialPort::BaudRate or any positive qint32 value.
+    If the setting is successful or set before opening the port, returns true;
+    otherwise returns false and sets an error code which can be obtained by
+    accessing the value of the QSerialPort::error property. To set the baud
+    rate, use the enumeration QSerialPort::BaudRate or any positive qint32
+    value.
 
-    \note The serial port has to be open before trying to set this property;
-    otherwise returns false and sets the NotOpenError error code. This is a bit
-    unusual as opposed to the regular Qt property settings of a class. However,
-    this is a special use case since the property is set through the interaction
-    with the kernel and hardware. Hence, the two scenarios cannot be completely
-    compared to each other.
+    \note If the setting is set before opening the port, the actual serial port
+    setting is done automatically in the \l{QSerialPort::open()} method right
+    after that the opening of the port succeeds.
 
     \warning Setting the AllDirections flag is only supported on
     the Windows, Windows CE, and Symbian platforms.
 
     \warning Returns equal baud rate in any direction on Windows, Windows CE, and
     Symbian.
+
+    The default value is Baud9600, i.e. 9600 bits per second.
 */
 bool QSerialPort::setBaudRate(qint32 baudRate, Directions directions)
 {
     Q_D(QSerialPort);
 
-    if (!isOpen()) {
-        setError(QSerialPort::NotOpenError);
-        qWarning("%s: device not open", Q_FUNC_INFO);
-        return false;
-    }
-
-    if (d->setBaudRate(baudRate, directions)) {
+    if (!isOpen() || d->setBaudRate(baudRate, directions)) {
         if (directions & QSerialPort::Input) {
             if (d->inputBaudRate != baudRate)
                 d->inputBaudRate = baudRate;
@@ -670,28 +672,21 @@ qint32 QSerialPort::baudRate(Directions directions) const
     \property QSerialPort::dataBits
     \brief the data bits in a frame
 
-    If the setting is successful, returns true; otherwise returns false and sets
-    an error code which can be obtained by accessing the value of the
-    QSerialPort::error property.
+    If the setting is successful or set before opening the port, returns
+    true; otherwise returns false and sets an error code which can be obtained
+    by accessing the value of the QSerialPort::error property.
 
-    \note The serial port has to be open before trying to set this property;
-    otherwise returns false and sets the NotOpenError error code. This is a bit
-    unusual as opposed to the regular Qt property settings of a class. However,
-    this is a special use case since the property is set through the interaction
-    with the kernel and hardware. Hence, the two scenarios cannot be completely
-    compared to each other.
+    \note If the setting is set before opening the port, the actual serial port
+    setting is done automatically in the \l{QSerialPort::open()} method right
+    after that the opening of the port succeeds.
+
+    The default value is Data8, i.e. 8 data bits.
 */
 bool QSerialPort::setDataBits(DataBits dataBits)
 {
     Q_D(QSerialPort);
 
-    if (!isOpen()) {
-        setError(QSerialPort::NotOpenError);
-        qWarning("%s: device not open", Q_FUNC_INFO);
-        return false;
-    }
-
-    if (d->setDataBits(dataBits)) {
+    if (!isOpen() || d->setDataBits(dataBits)) {
         if (d->dataBits != dataBits) {
             d->dataBits = dataBits;
             emit dataBitsChanged(d->dataBits);
@@ -722,28 +717,21 @@ QSerialPort::DataBits QSerialPort::dataBits() const
     \property QSerialPort::parity
     \brief the parity checking mode
 
-    If the setting is successful, returns true; otherwise returns false and sets
-    an error code which can be obtained by accessing the value of the
-    QSerialPort::error property.
+    If the setting is successful or set before opening the port, returns true;
+    otherwise returns false and sets an error code which can be obtained by
+    accessing the value of the QSerialPort::error property.
 
-    \note The serial port has to be open before trying to set this property;
-    otherwise returns false and sets the NotOpenError error code. This is a bit
-    unusual as opposed to the regular Qt property settings of a class. However,
-    this is a special use case since the property is set through the interaction
-    with the kernel and hardware. Hence, the two scenarios cannot be completely
-    compared to each other.
+    \note If the setting is set before opening the port, the actual serial port
+    setting is done automatically in the \l{QSerialPort::open()} method right
+    after that the opening of the port succeeds.
+
+    The default value is NoParity, i.e. no parity.
 */
 bool QSerialPort::setParity(Parity parity)
 {
     Q_D(QSerialPort);
 
-    if (!isOpen()) {
-        setError(QSerialPort::NotOpenError);
-        qWarning("%s: device not open", Q_FUNC_INFO);
-        return false;
-    }
-
-    if (d->setParity(parity)) {
+    if (!isOpen() || d->setParity(parity)) {
         if (d->parity != parity) {
             d->parity = parity;
             emit parityChanged(d->parity);
@@ -773,28 +761,21 @@ QSerialPort::Parity QSerialPort::parity() const
     \property QSerialPort::stopBits
     \brief the number of stop bits in a frame
 
-    If the setting is successful, returns true; otherwise returns false and
-    sets an error code which can be obtained by accessing the value of the
-    QSerialPort::error property.
+    If the setting is successful or set before opening the port, returns true;
+    otherwise returns false and sets an error code which can be obtained by
+    accessing the value of the QSerialPort::error property.
 
-    \note The serial port has to be open before trying to set this property;
-    otherwise returns false and sets the NotOpenError error code. This is a bit
-    unusual as opposed to the regular Qt property settings of a class. However,
-    this is a special use case since the property is set through the interaction
-    with the kernel and hardware. Hence, the two scenarios cannot be completely
-    compared to each other.
+    \note If the setting is set before opening the port, the actual serial port
+    setting is done automatically in the \l{QSerialPort::open()} method right
+    after that the opening of the port succeeds.
+
+    The default value is OneStop, i.e. 1 stop bit.
 */
 bool QSerialPort::setStopBits(StopBits stopBits)
 {
     Q_D(QSerialPort);
 
-    if (!isOpen()) {
-        setError(QSerialPort::NotOpenError);
-        qWarning("%s: device not open", Q_FUNC_INFO);
-        return false;
-    }
-
-    if (d->setStopBits(stopBits)) {
+    if (!isOpen() || d->setStopBits(stopBits)) {
         if (d->stopBits != stopBits) {
             d->stopBits = stopBits;
             emit stopBitsChanged(d->stopBits);
@@ -824,28 +805,21 @@ QSerialPort::StopBits QSerialPort::stopBits() const
     \property QSerialPort::flowControl
     \brief the desired flow control mode
 
-    If the setting is successful, returns true; otherwise returns false and sets
-    an error code which can be obtained by accessing the value of the
-    QSerialPort::error property.
+    If the setting is successful or set before opening the port, returns true;
+    otherwise returns false and sets an error code which can be obtained by
+    accessing the value of the QSerialPort::error property.
 
-    \note The serial port has to be open before trying to set this property;
-    otherwise returns false and sets the NotOpenError error code. This is a bit
-    unusual as opposed to the regular Qt property settings of a class. However,
-    this is a special use case since the property is set through the interaction
-    with the kernel and hardware. Hence, the two scenarios cannot be completely
-    compared to each other.
+    \note If the setting is set before opening the port, the actual serial port
+    setting is done automatically in the \l{QSerialPort::open()} method right
+    after that the opening of the port succeeds.
+
+    The default value is NoFlowControl, i.e. no flow control.
 */
 bool QSerialPort::setFlowControl(FlowControl flowControl)
 {
     Q_D(QSerialPort);
 
-    if (!isOpen()) {
-        setError(QSerialPort::NotOpenError);
-        qWarning("%s: device not open", Q_FUNC_INFO);
-        return false;
-    }
-
-    if (d->setFlowControl(flowControl)) {
+    if (!isOpen() || d->setFlowControl(flowControl)) {
         if (d->flowControl != flowControl) {
             d->flowControl = flowControl;
             emit flowControlChanged(d->flowControl);
@@ -875,15 +849,13 @@ QSerialPort::FlowControl QSerialPort::flowControl() const
     \property QSerialPort::dataTerminalReady
     \brief the state (high or low) of the line signal DTR
 
-    If the setting is successful, returns true; otherwise returns false.
-    If the flag is true then the DTR signal is set to high; otherwise low.
+    If the setting is successful or set before opening the port, returns true;
+    otherwise returns false. If the flag is true then the DTR signal is set to
+    high; otherwise low.
 
-    \note The serial port has to be open before trying to set or get this
-    property; otherwise returns false and sets the NotOpenError error code. This
-    is a bit unusual as opposed to the regular Qt property settings of a class.
-    However, this is a special use case since the property is set through the
-    interaction with the kernel and hardware. Hence, the two scenarios cannot be
-    completely compared to each other.
+    \note If the setting is set before opening the port, the actual serial port
+    setting is done automatically in the \l{QSerialPort::open()} method right
+    after that the opening of the port succeeds.
 
     \sa pinoutSignals()
 */
@@ -891,13 +863,7 @@ bool QSerialPort::setDataTerminalReady(bool set)
 {
     Q_D(QSerialPort);
 
-    if (!isOpen()) {
-        setError(QSerialPort::NotOpenError);
-        qWarning("%s: device not open", Q_FUNC_INFO);
-        return false;
-    }
-
-    bool retval = d->setDataTerminalReady(set);
+    bool retval = !isOpen() || d->setDataTerminalReady(set);
     if (retval && (d->dataTerminalReady != set)) {
         d->dataTerminalReady = set;
         emit dataTerminalReadyChanged(set);
@@ -926,15 +892,13 @@ bool QSerialPort::isDataTerminalReady()
     \property QSerialPort::requestToSend
     \brief the state (high or low) of the line signal RTS
 
-    If the setting is successful, returns true; otherwise returns false.
-    If the flag is true then the RTS signal is set to high; otherwise low.
+    If the setting is successful or set before opening the port, returns true;
+    otherwise returns false. If the flag is true then the RTS signal is set to
+    high; otherwise low.
 
-    \note The serial port has to be open before trying to set or get this
-    property; otherwise returns false and sets the NotOpenError error code. This
-    is a bit unusual as opposed to the regular Qt property settings of a class.
-    However, this is a special use case since the property is set through the
-    interaction with the kernel and hardware. Hence, the two scenarios cannot be
-    completely compared to each other.
+    \note If the setting is set before opening the port, the actual serial port
+    setting is done automatically in the \l{QSerialPort::open()} method right
+    after that the opening of the port succeeds.
 
     \sa pinoutSignals()
 */
@@ -942,13 +906,7 @@ bool QSerialPort::setRequestToSend(bool set)
 {
     Q_D(QSerialPort);
 
-    if (!isOpen()) {
-        setError(QSerialPort::NotOpenError);
-        qWarning("%s: device not open", Q_FUNC_INFO);
-        return false;
-    }
-
-    bool retval = d->setRequestToSend(set);
+    bool retval = !isOpen() || d->setRequestToSend(set);
     if (retval && (d->requestToSend != set)) {
         d->requestToSend = set;
         emit requestToSendChanged(set);

src/serialport/qserialport_symbian.cpp
@@ -149,7 +149,6 @@ bool QSerialPortPrivate::open(QIODevice::OpenMode mode)
         return false;
     }
 
-    detectDefaultSettings();
     return true;
 }
 
@@ -252,6 +251,11 @@ bool QSerialPortPrivate::waitForBytesWritten(int msec)
 {
     // TODO: Implement me
     return false;
+}
+
+bool QSerialPortPrivate::setBaudRate()
+{
+    return setBaudRate(inputBaudRate, QSerialPort::AllDirections);
 }
 
 bool QSerialPortPrivate::setBaudRate(qint32 baudRate, QSerialPort::Directions directions)
@@ -387,84 +391,6 @@ bool QSerialPortPrivate::updateCommConfig()
         return false;
     }
     return true;
-}
-
-void QSerialPortPrivate::detectDefaultSettings()
-{
-    // Detect baud rate.
-    inputBaudRate = baudRateFromSetting(currentSettings().iRate);
-    outputBaudRate = inputBaudRate;
-
-    // Detect databits.
-    switch (currentSettings().iDataBits) {
-    case EData5:
-        dataBits = QSerialPort::Data5;
-        break;
-    case EData6:
-        dataBits = QSerialPort::Data6;
-        break;
-    case EData7:
-        dataBits = QSerialPort::Data7;
-        break;
-    case EData8:
-        dataBits = QSerialPort::Data8;
-        break;
-    default:
-        qWarning("%s: Unexpected data bits settings", Q_FUNC_INFO);
-        dataBits = QSerialPort::Data8;
-        break;
-    }
-
-    // Detect parity.
-    switch (currentSettings().iParity) {
-    case EParityNone:
-        parity = QSerialPort::NoParity;
-        break;
-    case EParityEven:
-        parity = QSerialPort::EvenParity;
-        break;
-    case EParityOdd:
-        parity = QSerialPort::OddParity;
-        break;
-    case EParityMark:
-        parity = QSerialPort::MarkParity;
-        break;
-    case EParitySpace:
-        parity = QSerialPort::SpaceParity;
-        break;
-    default:
-        qWarning("%s: Unexpected parity settings", Q_FUNC_INFO);
-        parity = QSerialPort::NoParity;
-        break;
-    }
-
-    // Detect stopbits.
-    switch (currentSettings().iStopBits) {
-    case EStop1:
-        stopBits = QSerialPort::OneStop;
-        break;
-    case EStop2:
-        stopBits = QSerialPort::TwoStop;
-        break;
-    default:
-        qWarning("%s: Unexpected stop bits settings", Q_FUNC_INFO);
-        stopBits = QSerialPort::OneStop;
-        break;
-    }
-
-    // Detect flow control.
-    if ((currentSettings().iHandshake & (KConfigObeyXoff | KConfigSendXoff))
-            == (KConfigObeyXoff | KConfigSendXoff))
-        flowControl = QSerialPort::SoftwareControl;
-    else if ((currentSettings().iHandshake & (KConfigObeyCTS | KConfigFreeRTS))
-             == (KConfigObeyCTS | KConfigFreeRTS))
-        flowControl = QSerialPort::HardwareControl;
-    else if (currentSettings().iHandshake & KConfigFailDSR)
-        flowControl = QSerialPort::NoFlowControl;
-    else {
-        qWarning("%s: Unexpected flow control settings", Q_FUNC_INFO);
-        flowControl = QSerialPort::NoFlowControl;
-    }
 }
 
 QSerialPort::SerialPortError QSerialPortPrivate::decodeSystemError() const

src/serialport/qserialport_symbian_p.h
@@ -74,6 +74,7 @@ public:
     bool waitForReadyRead(int msec);
     bool waitForBytesWritten(int msec);
 
+    bool setBaudRate();
     bool setBaudRate(qint32 baudRate, QSerialPort::Directions directions);
     bool setDataBits(QSerialPort::DataBits dataBits);
     bool setParity(QSerialPort::Parity parity);
@@ -101,7 +102,6 @@ public:
 private:
     bool updateCommConfig();
 
-    void detectDefaultSettings();
     QSerialPort::SerialPortError decodeSystemError() const;
 
     bool waitForReadOrWrite(bool *selectForRead, bool *selectForWrite,

src/serialport/qserialport_unix_p.h
@@ -113,6 +113,7 @@ public:
     bool waitForReadyRead(int msecs);
     bool waitForBytesWritten(int msecs);
 
+    bool setBaudRate();
     bool setBaudRate(qint32 baudRate, QSerialPort::Directions directions);
     bool setDataBits(QSerialPort::DataBits dataBits);
     bool setParity(QSerialPort::Parity parity);
@@ -161,7 +162,6 @@ public:
 private:
     bool updateTermios();
 
-    void detectDefaultSettings();
     QSerialPort::SerialPortError decodeSystemError() const;
 
     bool isReadNotificationEnabled() const;

src/serialport/qserialport_win.cpp
@@ -208,7 +208,6 @@ bool QSerialPortPrivate::open(QIODevice::OpenMode mode)
 
     communicationNotifier->setEnabled(true);
 
-    detectDefaultSettings();
     return true;
 }
 
@@ -460,6 +459,11 @@ bool QSerialPortPrivate::waitForBytesWritten(int msecs)
 }
 
 #endif // #ifndef Q_OS_WINCE
+
+bool QSerialPortPrivate::setBaudRate()
+{
+    return setBaudRate(inputBaudRate, QSerialPort::AllDirections);
+}
 
 bool QSerialPortPrivate::setBaudRate(qint32 baudRate, QSerialPort::Directions directions)
 {
@@ -792,80 +796,6 @@ bool QSerialPortPrivate::updateCommTimeouts()
 }
 
 #endif // #ifndef Q_OS_WINCE
-
-void QSerialPortPrivate::detectDefaultSettings()
-{
-    // Detect baud rate.
-    inputBaudRate = quint32(currentDcb.BaudRate);
-    outputBaudRate = inputBaudRate;
-
-    // Detect databits.
-    switch (currentDcb.ByteSize) {
-    case 5:
-        dataBits = QSerialPort::Data5;
-        break;
-    case 6:
-        dataBits = QSerialPort::Data6;
-        break;
-    case 7:
-        dataBits = QSerialPort::Data7;
-        break;
-    case 8:
-        dataBits = QSerialPort::Data8;
-        break;
-    default:
-        qWarning("%s: Unexpected data bits settings", Q_FUNC_INFO);
-        dataBits = QSerialPort::Data8;
-        break;
-    }
-
-    // Detect parity.
-    if ((currentDcb.Parity == NOPARITY) && !currentDcb.fParity)
-        parity = QSerialPort::NoParity;
-    else if ((currentDcb.Parity == SPACEPARITY) && currentDcb.fParity)
-        parity = QSerialPort::SpaceParity;
-    else if ((currentDcb.Parity == MARKPARITY) && currentDcb.fParity)
-        parity = QSerialPort::MarkParity;
-    else if ((currentDcb.Parity == EVENPARITY) && currentDcb.fParity)
-        parity = QSerialPort::EvenParity;
-    else if ((currentDcb.Parity == ODDPARITY) && currentDcb.fParity)
-        parity = QSerialPort::OddParity;
-    else {
-        qWarning("%s: Unexpected parity settings", Q_FUNC_INFO);
-        parity = QSerialPort::NoParity;
-    }
-
-    // Detect stopbits.
-    switch (currentDcb.StopBits) {
-    case ONESTOPBIT:
-        stopBits = QSerialPort::OneStop;
-        break;
-    case ONE5STOPBITS:
-        stopBits = QSerialPort::OneAndHalfStop;
-        break;
-    case TWOSTOPBITS:
-        stopBits = QSerialPort::TwoStop;
-        break;
-    default:
-        qWarning("%s: Unexpected stop bits settings", Q_FUNC_INFO);
-        stopBits = QSerialPort::OneStop;
-        break;
-    }
-
-    // Detect flow control.
-    if (!currentDcb.fOutxCtsFlow && (currentDcb.fRtsControl == RTS_CONTROL_DISABLE)
-            && !currentDcb.fInX && !currentDcb.fOutX) {
-        flowControl = QSerialPort::NoFlowControl;
-    } else if (!currentDcb.fOutxCtsFlow && (currentDcb.fRtsControl == RTS_CONTROL_DISABLE)
-               && currentDcb.fInX && currentDcb.fOutX) {
-        flowControl = QSerialPort::SoftwareControl;
-    } else if (currentDcb.fOutxCtsFlow && (currentDcb.fRtsControl == RTS_CONTROL_HANDSHAKE)
-               && !currentDcb.fInX && !currentDcb.fOutX) {
-        flowControl = QSerialPort::HardwareControl;
-    } else {
-        flowControl = QSerialPort::NoFlowControl;
-    }
-}
 
 QSerialPort::SerialPortError QSerialPortPrivate::decodeSystemError() const
 {

src/serialport/qserialport_win_p.h
@@ -83,6 +83,7 @@ public:
     bool waitForReadyRead(int msec);
     bool waitForBytesWritten(int msec);
 
+    bool setBaudRate();
     bool setBaudRate(qint32 baudRate, QSerialPort::Directions directions);
     bool setDataBits(QSerialPort::DataBits dataBits);
     bool setParity(QSerialPort::Parity parity);
@@ -144,7 +145,6 @@ private:
     bool updateDcb();
     bool updateCommTimeouts();
 
-    void detectDefaultSettings();
 
 #ifndef Q_OS_WINCE
     bool waitAnyEvent(int msecs, bool *timedOut, HANDLE *triggeredEvent);

src/serialport/qserialport_wince.cpp
@@ -224,7 +224,6 @@ bool QSerialPortPrivate::open(QIODevice::OpenMode mode)
     eventNotifier = new CommEventNotifier(eventMask, this, q);
     eventNotifier->start();
 
-    detectDefaultSettings();
     return true;
 }
 

commit 3d19812f745fac35aab02743c576a51dd9b0714f

Fix setting custom baud rates under Linux
This regression was caused by 7c737e0edcb93585856c65890ef34e5c5a28ee6b.
The initialization of the (current) serial port info was removed in that commit.

Tested on Linux x86_64 with Qt 5.1.1 and 4.8.6.

Task-number: QTBUG-38961
Change-Id: Idd3c5dc7eb37093848f33387563ff14567ac54fe
Reviewed-by: Denis Shienkov <denis.shienkov@gmail.com>
Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@digia.com>
Reviewed-by: Rafael Roquetto <rafael.roquetto@kdab.com>

src/serialport/qserialport_unix.cpp
@@ -260,6 +260,11 @@ bool QSerialPortPrivate::open(QIODevice::OpenMode mode)
     if (!updateTermios())
         return false;
 
+#ifdef Q_OS_LINUX
+    isCustomBaudRateSupported = (::ioctl(descriptor, TIOCGSERIAL, &currentSerialInfo) != -1)
+            && (::ioctl(descriptor, TIOCSSERIAL, &currentSerialInfo) != -1);
+#endif
+
     setExceptionNotificationEnabled(true);
 
     if ((flags & O_WRONLY) == 0)