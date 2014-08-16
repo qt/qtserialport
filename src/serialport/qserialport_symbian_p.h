286d64c202f461cb8ff5a4cc4c2320af8195b8f0
Samuel Gaist <samuel.gaist@edeltech.ch>
2014-08-16 23:24:32 +0200

    Add missing private headers warnings
    Change-Id: Ia60bf0d020d98a6fda8f689d631bb8681950d15f
    Reviewed-by: Sergey Belyashov <Sergey.Belyashov@gmail.com>

--- src/serialport/qserialport_symbian_p.h
+++ src/serialport/qserialport_symbian_p.h
@@ -42,6 +42,17 @@
 #ifndef QSERIALPORT_SYMBIAN_P_H
 #define QSERIALPORT_SYMBIAN_P_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include "qserialport_p.h"
 
 #include <c32comm.h>
--- src/serialport/qtudev_p.h
+++ src/serialport/qtudev_p.h
@@ -42,6 +42,17 @@
 #ifndef QTUDEV_P_H
 #define QTUDEV_P_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #ifdef LINK_LIBUDEV
 extern "C"
 {
