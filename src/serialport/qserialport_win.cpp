93d387a375abfed85c3ce37b68c80f3efd04ea6d
Denis Shienkov <denis.shienkov@gmail.com>
2014-11-21 23:36:57 +0300

    Declare QElapsedTimer closer to usage
    Change-Id: I74622eaf3a7f5179ff25fc815997d5e4cf3cc2c7
    Reviewed-by: Denis Shienkov <denis.shienkov@gmail.com>

--- src/serialport/qserialport_win.cpp
+++ src/serialport/qserialport_win.cpp
@@ -304,14 +304,14 @@ bool QSerialPortPrivate::waitForReadyRead(int msecs)
 {
     Q_Q(QSerialPort);
 
-    QElapsedTimer stopWatch;
-    stopWatch.start();
-
     if (!writeStarted && !_q_startAsyncWrite())
         return false;
 
     const qint64 initialReadBufferSize = readBuffer.size();
     qint64 currentReadBufferSize = initialReadBufferSize;
+
+    QElapsedTimer stopWatch;
+    stopWatch.start();
 
     do {
         bool timedOut = false;
@@ -358,11 +358,11 @@ bool QSerialPortPrivate::waitForBytesWritten(int msecs)
     if (writeBuffer.isEmpty())
         return false;
 
-    QElapsedTimer stopWatch;
-    stopWatch.start();
-
     if (!writeStarted && !_q_startAsyncWrite())
         return false;
+
+    QElapsedTimer stopWatch;
+    stopWatch.start();
 
     forever {
         bool timedOut = false;
