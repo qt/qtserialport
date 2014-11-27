ae99420608c699de002032c751192065447e1242
Andy Shaw <andy.shaw@digia.com>
2014-11-27 12:24:17 +0100

    Close the handle for the events that were created
    The handles should be closed when QSerialPort is deleted in order to clean
    up the native resources.
    
    Change-Id: If04521fc0fa3b898093bda3317962b4c44818034
    Reviewed-by: Denis Shienkov <denis.shienkov@gmail.com>

--- src/serialport/qserialport_win_p.h
+++ src/serialport/qserialport_win_p.h
@@ -63,6 +63,7 @@ class QSerialPortPrivate : public QSerialPortPrivateData
 
 public:
     QSerialPortPrivate(QSerialPort *q);
+    ~QSerialPortPrivate();
 
     bool open(QIODevice::OpenMode mode);
     void close();
