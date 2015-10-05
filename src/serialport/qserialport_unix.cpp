qt/qtserialport

commit ba204a95e6650a6992510530c1fa5a4dac2b5da8

Fix stalling of reading with the limited buffer size on *nix
When the read buffer with a limited size is used, the read notifier
becomes disabled when the buffer completely is filled. The notifier
should be enabled again when several bytes were read from the read
buffer.

(cherry-picked from 23c89518d1404a4db570fe41f3cc04451703e16f)
Change-Id: I723253fb153a1144009579141fa0ce1aca93b648
Reviewed-by: Denis Shienkov <denis.shienkov@gmail.com>

src/serialport/qserialport_unix.cpp
@@ -395,7 +395,12 @@ bool QSerialPortPrivate::setBreakEnabled(bool set)
 
 qint64 QSerialPortPrivate::readData(char *data, qint64 maxSize)
 {
-    return readBuffer.read(data, maxSize);
+    const qint64 result = readBuffer.read(data, maxSize);
+    // We need try to re-trigger the read notification to read a remainder from a
+    // driver's queue in case we have a limited read buffer size.
+    if (d->readBufferMaxSize && result > 0 && !d->isReadNotificationEnabled())
+        d->setReadNotificationEnabled(true);
+    return result;
 }
 
 bool QSerialPortPrivate::waitForReadyRead(int msecs)
@@ -744,6 +749,7 @@ bool QSerialPortPrivate::readNotification()
         if (bytesToRead == 0) {
             // Buffer is full. User must read data from the buffer
             // before we can read more from the port.
+            setReadNotificationEnabled(false);
             return false;
         }
     }

tests/auto/qserialport/tst_qserialport.cpp
@@ -129,6 +129,8 @@ private slots:
     void asynchronousWriteByTimer_data();
     void asynchronousWriteByTimer();
 
+    void asyncReadWithLimitedReadBufferSize();
+
     void readBufferOverflow();
     void readAfterInputClear();
     void synchronousReadWriteAfterAsynchronousReadWrite();
@@ -805,6 +807,49 @@ void tst_QSerialPort::asynchronousWriteByTimer()
     QVERIFY2(!timeout(), "Timed out when waiting for the read or write.");
     QCOMPARE(receiverPort.bytesAvailable(), qint64(alphabetArray.size()));
     QCOMPARE(receiverPort.readAll(), alphabetArray);
+}
+
+class AsyncReader2 : public QObject
+{
+    Q_OBJECT
+public:
+    explicit AsyncReader2(QSerialPort &port, const QByteArray &expectedData)
+        : serialPort(port), expectedData(expectedData)
+    {
+        connect(&serialPort, SIGNAL(readyRead()), this, SLOT(receive()));
+    }
+
+private slots:
+    void receive()
+    {
+        receivedData.append(serialPort.readAll());
+        if (receivedData == expectedData)
+            tst_QSerialPort::exitLoop();
+    }
+
+private:
+    QSerialPort &serialPort;
+    const QByteArray expectedData;
+    QByteArray receivedData;
+};
+
+void tst_QSerialPort::asyncReadWithLimitedReadBufferSize()
+{
+    QSerialPort senderPort(m_senderPortName);
+    QVERIFY(senderPort.open(QSerialPort::WriteOnly));
+
+    QSerialPort receiverPort(m_receiverPortName);
+    QVERIFY(receiverPort.open(QSerialPort::ReadOnly));
+
+    receiverPort.setReadBufferSize(1);
+    QCOMPARE(receiverPort.readBufferSize(), qint64(1));
+
+    AsyncReader2 reader(receiverPort, alphabetArray);
+
+    QCOMPARE(senderPort.write(alphabetArray), qint64(alphabetArray.size()));
+
+    enterLoop(1);
+    QVERIFY2(!timeout(), "Timed out when waiting for the read or write.");
 }
 
 void tst_QSerialPort::readBufferOverflow()

commit 796bd6f411af2502032c7bbd4c41fc891be04079

Fix build on *nix
This fixes the build error which introduced from
ba204a95e6650a6992510530c1fa5a4dac2b5da8

Change-Id: I66a8a059826ede47f5ace9b86970bf91a2de1e26
Reviewed-by: Denis Shienkov <denis.shienkov@gmail.com>

src/serialport/qserialport_unix.cpp
@@ -398,8 +398,8 @@ qint64 QSerialPortPrivate::readData(char *data, qint64 maxSize)
     const qint64 result = readBuffer.read(data, maxSize);
     // We need try to re-trigger the read notification to read a remainder from a
     // driver's queue in case we have a limited read buffer size.
-    if (d->readBufferMaxSize && result > 0 && !d->isReadNotificationEnabled())
-        d->setReadNotificationEnabled(true);
+    if (readBufferMaxSize && result > 0 && !isReadNotificationEnabled())
+        setReadNotificationEnabled(true);
     return result;
 }
 