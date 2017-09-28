90081a1b3698375067a2177048df42a88f089dd9
Andre Hartmann <aha_1980@gmx.de>
2017-09-28 18:20:23 +0200

    Update waitFor{ReadyRead|BytesWritten} documentation for timeouts
    [ChangeLog][QSerialPort] Added a note to the documentation that
    waitForReadyRead() and waitForBytesWritten() never time out if
    the parameter msecs is -1.
    
    Change-Id: I5852da14faf1bba5a338db6a8764da582238aae7
    Reviewed-by: Denis Shienkov <denis.shienkov@gmail.com>

--- src/serialport/qserialport.cpp
+++ src/serialport/qserialport.cpp
@@ -1309,7 +1309,7 @@ bool QSerialPort::canReadLine() const
     This function blocks until new data is available for reading and the
     \l{QIODevice::}{readyRead()} signal has been emitted. The function
     will timeout after \a msecs milliseconds; the default timeout is
-    30000 milliseconds.
+    30000 milliseconds. If \a msecs is -1, this function will not time out.
 
     The function returns \c true if the readyRead() signal is emitted and
     there is new data available for reading; otherwise it returns \c false
@@ -1341,7 +1341,7 @@ bool QSerialPort::waitForReadyRead(int msecs)
     This function blocks until at least one byte has been written to the serial
     port and the \l{QIODevice::}{bytesWritten()} signal has been emitted. The
     function will timeout after \a msecs milliseconds; the default timeout is
-    30000 milliseconds.
+    30000 milliseconds. If \a msecs is -1, this function will not time out.
 
     The function returns \c true if the bytesWritten() signal is emitted; otherwise
     it returns \c false (if an error occurred or the operation timed out).
