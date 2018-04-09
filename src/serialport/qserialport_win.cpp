qt/qtserialport

commit d57b9e83857253a1479f89fca18ea5856db39fb7

Create notifier only in end of successful initialization
... otherwise, this notifier's instance will be active even
if the serial port is not open.

Change-Id: Ieebb894a048f3ed89ac5d1c259e21bc790f5d387
Reviewed-by: Anton Kudryavtsev <antkudr@mail.ru>
Reviewed-by: Denis Shienkov <denis.shienkov@gmail.com>

src/serialport/qserialport_win.cpp
@@ -694,14 +694,14 @@ inline bool QSerialPortPrivate::initialize(QIODevice::OpenMode mode)
         return false;
     }
 
+    if ((eventMask & EV_RXCHAR) && !startAsyncCommunication())
+        return false;
+
     notifier = new QWinOverlappedIoNotifier(q);
     QObjectPrivate::connect(notifier, &QWinOverlappedIoNotifier::notified,
                this, &QSerialPortPrivate::_q_notified);
     notifier->setHandle(handle);
     notifier->setEnabled(true);
-
-    if ((eventMask & EV_RXCHAR) && !startAsyncCommunication())
-        return false;
 
     return true;
 }

commit 0c988e14a90f3e1579365dc3dc99fbdd02ac3d4c

Start overlapped I/O notifier before any asynchronous operation
This commit d57b9e83857253a1479f89fca18ea5856db39fb7 introduced
a regression, where the QSP may ignore all the read events when
the data comes to the device within opening. In this case, even
re-opening of a device does not help.

Reason is that the QWinOverlappedIoNotifier is enabled after than
the startAsyncCommunication() called, that probably, leads to
ignoring for all EV_RXCHAR events. A workaround is to enable the
notifier before than any of I/O operation called.

Task-number: QTBUG-67224
Change-Id: I21538fddc4dfd850c600e1d74f3c393a59a00549
Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>
Reviewed-by: Oliver Wolff <oliver.wolff@qt.io>
Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@qt.io>

src/serialport/qserialport_win.cpp
@@ -691,14 +691,17 @@ inline bool QSerialPortPrivate::initialize(QIODevice::OpenMode mode)
         return false;
     }
 
-    if ((eventMask & EV_RXCHAR) && !startAsyncCommunication())
-        return false;
-
     notifier = new QWinOverlappedIoNotifier(q);
     QObjectPrivate::connect(notifier, &QWinOverlappedIoNotifier::notified,
                this, &QSerialPortPrivate::_q_notified);
     notifier->setHandle(handle);
     notifier->setEnabled(true);
+
+    if ((eventMask & EV_RXCHAR) && !startAsyncCommunication()) {
+        delete notifier;
+        notifier = nullptr;
+        return false;
+    }
 
     return true;
 }