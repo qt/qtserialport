79f3aee73d576e0f91d44fa6781edd3105632a2a
Ivan Solovev <ivan.solovev@qt.io>
2026-09-21 12:34:51 +0200

    Rework QSerialPort::setWriteBufferSize() docs
    Fix the wording as suggested by AI, and fix one actual mistake - when
    talking about passing 0, the documentation was mentioning an input
    buffer, not the write buffer. Change it.
    
    Amends 07d21cb06765c31d3f7da39991d086dcd8719f26 (6.10)
    
    Change-Id: I95a2436d39893f007d077051f3064ea4b0ff8fd2
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>
    (cherry picked from commit 4bd1bd57f9baaca44e81fc8240bca556760cfe86)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/serialport/qserialport.cpp
+++ src/serialport/qserialport.cpp
@@ -1062,24 +1062,24 @@ qint64 QSerialPort::writeBufferSize() const
     Sets the size of QSerialPort's internal write buffer to be \a
     size bytes.
 
-    Sending the data over serial port is relatively slow, so in practice, when
-    \l write() is called, the data is not sent immediately. It is first stored
-    in an intermediate buffer and later written in chunks.
+    Sending data over a serial port is relatively slow, so in practice the data
+    is not sent immediately when \l write() is called. It is first stored in an
+    intermediate buffer and later written in chunks.
 
-    Thus, an attempt to write too much data or write data at a higher rate than
-    the underlying serial port can handle, can lead to a situation when the
-    internal buffer will grow. This can eventually cause the application to
-    run out of memory, specially on a device with low memory resources.
+    Thus, writing too much data, or writing it at a higher rate than the
+    underlying serial port can handle, can make the internal buffer grow. This
+    can eventually cause the application to run out of memory, especially on a
+    device with low memory resources.
 
-    This method allows to limit the internal buffer to a certain size. If the
+    This method allows limiting the internal buffer to a certain size. If the
     next write attempt exceeds the capacity of the buffer, the \l write()
-    method will return the amount of bytes that were actually stored in the
-    buffer. It's the user's responsibility to repeat the write attempt with the
-    rest of the bytes after the \l bytesWritten() signal was received, or after
-    the \l waitForBytesWritten() method returns \c true.
+    method returns the number of bytes that were actually stored in the buffer.
+    It is the user's responsibility to repeat the write attempt with the rest
+    of the bytes after the \l bytesWritten() signal is received, or after the
+    \l waitForBytesWritten() method returns \c true.
 
-    Passing \c 0 to this method means that the input buffer is not limited, and
-    all the incoming data is buffered. This is the default.
+    Passing \c 0 to this method means that the write buffer is not limited, and
+    all the data passed to \l write() is buffered. This is the default.
 
     Passing a negative value has the same effect as passing \c 0.
 
